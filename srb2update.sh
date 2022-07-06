a7a3d5dbc064343f7c25c07981e8cc2a3e5b5494
pinkcreeper100 <35699052+pinkcreeper100@users.noreply.github.com>
2022-07-06 19:46:43 +0100

    Create srb2update.sh

---
+++ srb2update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd "$HOME/srb2bld"
+git pull
+sudo make install
+cd "$HOME/srb2srv"
+git pull
+sudo make install
+cd "$HOME/srb2dl"
+git pull
+sudo make install
+cd "$HOME"
+srb2bld --update
+srb2dl --update
