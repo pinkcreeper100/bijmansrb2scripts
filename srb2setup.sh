fb278594a7556767dfe254c5e5663a9fce43d34d
pinkcreeper100 <35699052+pinkcreeper100@users.noreply.github.com>
2022-07-06 19:45:34 +0100

    Create srb2setup.sh

---
+++ srb2setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd "$HOME"
+git clone https://github.com/Bijman/srb2bld
+cd "$HOME/srb2bld"
+sudo make install
+cd "$HOME"
+git clone https://github.com/Bijman/srb2srv
+cd "$HOME/srb2srv"
+sudo make install
+cd "$HOME"
+git clone https://github.com/Bijman/srb2dl
+cd "$HOME/srb2dl"
+sudo make install
+cd "$HOME"
