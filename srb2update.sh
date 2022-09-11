aee7753bddc3703c08a4fad054c0820a7bdbfd89
pinkcreeper100 <benmoreosm@gmail.com>
2022-09-11 09:22:52 +0100

    refactor

--- srb2update.sh
+++ srb2update.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-cd "$HOME/srb2bld"
-git pull
-sudo make install
-cd "$HOME/srb2srv"
-git pull
-sudo make install
-cd "$HOME/srb2dl"
-git pull
-sudo make install
+cd "$HOME/srb2bld" && git pull && sudo make install
+cd "$HOME/srb2srv" && git pull && sudo make install
+cd "$HOME/srb2dl" && git pull && sudo make install
 cd "$HOME"
-srb2bld --update
-srb2dl --update
+srb2bld --update && srb2dl --update
