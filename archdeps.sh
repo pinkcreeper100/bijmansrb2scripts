459faedb6dfb560aa776b3b31728aaa6d87abc8f
pinkcreeper100 <benmoreosm@gmail.com>
2022-09-18 08:36:48 +0100

    group

--- archdeps.sh
+++ archdeps.sh
@@ -1,4 +1,5 @@
 echo "This will reboot your system! To cancel, press Ctrl+C" && sleep 10
 sudo pacman -S --needed make git which coreutils findutils ncurses curl gawk docker stow fuse patchelf
 sudo systemctl enable docker.service
+sudo usermod -aG docker $(whoami)
 reboot
